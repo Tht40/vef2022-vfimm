20535e60325082bb3c8c17024a9952e761276956
Tht40 <77277019+Tht40@users.noreply.github.com>
2022-04-08 23:24:43 +0000

    næstsiðasta

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'https://vef2-20222-v3-synilausn.herokuapp.com/' })
 }
---
+++ public/login.tsx
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+import {useRouter} from 'next/router'
+import styles from '../styles/Home.module.css'
+
+
+
+export function Login({
+  loggedin = false,
+  name,
+  onRegister,
+}) {
+
+  const router = useRouter();
+  
+  function onLogout(){
+    window.localStorage.removeItem("token")
+    router.push('/');
+  }
+  
+  if(typeof window !== 'undefined'){
+    if(window.localStorage.getItem("token")){
+      loggedin = true;
+      
+    }
+    else loggedin = false;
+  }
+  
+
+  if (loggedin) {
+    return (
+      <>
+        <p className={styles.footers}>
+          Skráður inn sem <strong>`Bearer ${window.localStorage.getItem('token')}`</strong>
+        </p>
+        <button className={styles.footers} onClick={onLogout}>Útskrá</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <p className={styles.back}>
+        <a href="/login">Innskráning</a>
+      </p>
+      <p className={styles.back}>
+        <a href="/register">Nýskráning</a>
+      </p>
+    </>
+  );
+}
+
+Login.propTypes = {
+  loggedin: PropTypes.bool,
+  onRegister: PropTypes.func,
+  name: PropTypes.string,
+};
